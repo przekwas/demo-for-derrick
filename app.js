3ecf809852f12ebac448a28e62fc2f66b848f166
przekwas <luke@covalence.io>
2019-12-16 22:44:29 -0600

    testing new setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let footer = document.getElementsByTagName('footer')[0];
-
-let btn = document.createElement('button');
-btn.textContent = 'Thanks!';
-btn.className = 'btn btn-primary d-inline-block mx-auto p-3 shadow-lg';
-footer.appendChild(btn);
+if (condition) {
+	if (condition) {
+	} else {
+	}
+} else {
+}
